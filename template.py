30e22e60eef7f6c5bc9e9c6e337cb3ab3449b784
Jeremy <jeremy.bluhm@protonmail.com>
2023-12-05 00:00:06 +0100

    update template

--- template.py
+++ template.py
@@ -6,10 +6,12 @@ def get_input(file):
     return input
 
 def solution_part1(input):
-    return input
+    sum = 0
+    return sum
 
 def solution_part2(input):
-    return input
+    sum = 0
+    return sum
 
 input = get_input('./input')
 print(f'The Solution of part 1 is: {solution_part1(input)}')
